9f060ba3eb127221329c33a4ca17966c22973ead
aventuracode <arielventura.code@gmail.com>
2025-12-14 12:26:00 -0300

    se agrega Swagger

--- RemiteeChallenge/Remitee.WebApi/Startup.cs
+++ RemiteeChallenge/Remitee.WebApi/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLogic.Logic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 using Remitee.Business.Data;
 using Remitee.Business.Logic;
 using Remitee.Core.Interfaces;
@@ -27,6 +28,26 @@ namespace Remitee.WebApi
             
             services.AddTransient<ILibroRepository, LibroRepository>();
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "Remitee API", 
+                    Version = "v1",
+                    Description = "API para la gestión de libros y categorías",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Remitee Team",
+                        Email = "support@remitee.com"
+                    }
+                });
+
+                var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
             services.AddCors(opt =>
             {
                 opt.AddPolicy("CorsRule", rule =>
@@ -39,6 +60,13 @@ namespace Remitee.WebApi
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Remitee API V1");
+                c.RoutePrefix = string.Empty; 
+            });
 
             app.UseStatusCodePagesWithReExecute("/errors", "?code={0}");
             app.UseRouting();
